88c4dbe34c357baff29b25e97674272ff226c057
João nunes <126252207+Joaosilva07@users.noreply.github.com>
2025-04-14 09:48:56 -0300

    Comments

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const PORT = 3000;
 
 // PostgreSQL: conexão com Pool
 const pool = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'sua_senha',
-    database: process.env.DB_DATABASE || 'iot_energy'
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT ,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_DATABASE,
 });
 
 // Middleware
@@ -50,6 +50,31 @@ app.post('/login', async (req, res) => {
     }
 });
 
+// rota register
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    
+    // Verificar se os campos foram preenchidos
+    if (!username || !password) {
+        return res.status(400).send('Usuário e senha são obrigatórios!');
+    }
+
+    // Verificar se o usuário já existe (simulando com um array)
+    const userExists = users.find(user => user.username === username);
+    if (userExists) {
+        return res.status(400).send('Usuário já existe!');
+    }
+
+    // Encriptar a senha
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Salvar usuário (aqui simulando um array, mas no caso de um banco de dados, seria um `INSERT`)
+    const newUser = { username, password: hashedPassword };
+    users.push(newUser);
+
+    return res.status(201).send('Usuário registrado com sucesso!');
+});
+
 // Rota para obter o nome do usuário logado
 app.get('/user-info', (req, res) => {
     if (req.session.username) {
@@ -59,7 +84,7 @@ app.get('/user-info', (req, res) => {
     }
 });
 
-// Rota de logout
+// Rota de logout caso houver
 app.post('/logout', (req, res) => {
     req.session.destroy();
     res.json({ success: true });
